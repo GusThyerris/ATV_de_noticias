f17510dc7580b38047decac0141fe414e5840ede
gustavo <127103850+gusrapaiva@users.noreply.github.com>
2023-10-26 21:00:32 -0300

    Add files via upload

--- ATV_Noticias/database/beluganews-DB.sql
+++ ATV_Noticias/database/beluganews-DB.sql
@@ -4,22 +4,11 @@ use beluganews;
 create table usuario (	id int auto_increment primary key,
 						nome varchar(35) not null,
 						tipo char(1) not null,
-						ativo boolean
-);
-
-insert into usuario values (0, 'gustavo paiva', 0, 1);
-
-select * from usuario;
-
-select * from usuario u inner join login l on u.id = l.id;
-
-create table login (id int,
-					email varchar(35) unique not null,
-                    senha char(8) not null,
-                    foreign key (id) references usuario (id)
+                        email varchar(35) unique not null,
+                        senha char(8) not null,
+                        ativo boolean
 );
  
-insert into login values (1, 'gustavo@gmail.com', 1234);
 
 create table noticia (	num_noticia int auto_increment primary key,
 						tipo char(1) not null,
@@ -30,3 +19,9 @@ create table noticia (	num_noticia int auto_increment primary key,
                         foreign key (id_autor) references usuario (id),
                         foreign key (id_editor) references usuario (id)
 );
+
+insert into usuario (nome, tipo, email, senha, ativo) values ('Admin', 0, 'admin@gmail.com', 1234, 1);
+
+select * from usuario;
+
+select id from usuario;
